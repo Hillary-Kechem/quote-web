f284394471d32d626681d94e58a4c864b1efe9af
Hillary-Kechem <hillarykechem92@gmail.com>
2022-02-10 11:35:48 +0300

    delete and add button

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,7 +13,20 @@ export class QuoteComponent implements OnInit {
     new Quote("Hillary","Moringa is the best place to be","Ms Chloe")
   ];
 
-  
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id=quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote);
+  }
+
+  //delete button 
+  completeQuote(isComplete, index){
+    if (isComplete){
+      this.quotes.splice(index, 1);
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
